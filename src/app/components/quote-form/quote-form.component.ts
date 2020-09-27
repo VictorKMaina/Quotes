9799db245d8a1205f116381dffa058678d8fae48
Victor K. Maina <vk13runic@gmail.com>
2020-09-28 01:52:26 +0300

    add logic for form submission

--- src/app/components/quote-form/quote-form.component.ts
+++ src/app/components/quote-form/quote-form.component.ts
@@ -14,6 +14,7 @@ export class QuoteFormComponent implements OnInit {
   submitQuote(){
     let tempQuote = Object.assign({}, this.newQuote)
     this.addNewQuote.emit(tempQuote);
+    
   }
 
   constructor() { }

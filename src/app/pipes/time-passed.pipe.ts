2ac162d86294388610eb3174fce3a0c3c9f8e486
Victor K. Maina <vk13runic@gmail.com>
2020-09-28 07:39:52 +0300

    change timePassed pipe to check for minutes and seconds

--- src/app/pipes/time-passed.pipe.ts
+++ src/app/pipes/time-passed.pipe.ts
@@ -9,6 +9,13 @@ export class TimePassedPipe implements PipeTransform {
   transform(entryTime: Date) {
     let currentTime = new Date().getTime();
     let timePassed = currentTime - entryTime.getTime();
-    return Math.round(timePassed/60000);
+    let time = Math.round(timePassed/60000)
+    if (time === 0){
+      return " less than a minute";
+    } if (time === 1) {
+      return time + " minute";
+    } else {
+      return time + " minutes";
+    }
   }
 }
